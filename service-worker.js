f02dacb5d78763c8e875367614e1a6b288109add
j0t4-w4r0 <70350516+j0t4-w4r0@users.noreply.github.com>
2026-09-06 17:30:49 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const NOMBRE_CACHE = "liquiruta-v1";
+
+const ARCHIVOS_A_GUARDAR = [
+  "index.html",
+  "nueva-ruta.html",
+  "ruta.html",
+  "registro-facturas.html",
+  "gastos.html",
+  "notas.html",
+  "conteo-billetes.html",
+  "liquidacion-final.html",
+  "editar-ruta.html",
+  "historial.html",
+  "descuento.html",
+  "css/estilos.css",
+  "js/core.js",
+  "js/db.js",
+  "js/logo.js",
+  "manifest.json",
+  "icons/icon-192.png",
+  "icons/icon-512.png",
+];
+
+self.addEventListener("install", (evento) => {
+  evento.waitUntil(
+    caches.open(NOMBRE_CACHE).then((cache) => cache.addAll(ARCHIVOS_A_GUARDAR))
+  );
+});
+
+self.addEventListener("activate", (evento) => {
+  evento.waitUntil(
+    caches.keys().then((nombres) =>
+      Promise.all(
+        nombres
+          .filter((nombre) => nombre !== NOMBRE_CACHE)
+          .map((nombre) => caches.delete(nombre))
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (evento) => {
+  evento.respondWith(
+    caches.match(evento.request).then((respuestaGuardada) => {
+      return respuestaGuardada || fetch(evento.request);
+    })
+  );
+});
